04f11e9e82f635c75bbb6707075b59255dd9d659
Rabia Sarikaya <rabiasarikaya@Rabias-MacBook-Pro.local>
2020-08-06 20:32:23 -0500

    Initial commit

---
+++ src/Day02_PrintStatements/HelloCybertek.java
@@ -0,0 +1,18 @@
+package Day02_PrintStatements;
+
+import java.util.Scanner;
+
+public class HelloCybertek {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cybertek");
+        System.out.println("");
+        System.out.println("");
+
+
+
+
+    }
+
+}
